fb22ce9d4cbaf707603629cd911ff67ab00e06e9
Monnika <128911763+Monnika23@users.noreply.github.com>
2023-03-26 23:44:09 +0530

    Create lottie.js

---
+++ lottie.js
@@ -0,0 +1,2 @@
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets4.lottiefiles.com/packages/lf20_2pezcshb.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop controls autoplay></lottie-player>
